98123a3db4bef8525d54adf8cd57dd54a877950a
Anukool Vikram <anukoolvikram@Anukools-MacBook-Pro.local>
2025-11-20 00:31:09 +0530

    Added Redis configurations

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -3,6 +3,8 @@ import IORedis from 'ioredis';
 const redisConfig = {
   host: process.env.REDIS_HOST || 'localhost',
   port: parseInt(process.env.REDIS_PORT || '6379'),
+  password: process.env.REDIS_PASSWORD || undefined,
+  username: process.env.REDIS_USER || undefined,
   maxRetriesPerRequest: null
 };
 
